13c278c3e2e76fe98f9b38852f70afacb3523030
Minory <30800424+minoryorg@users.noreply.github.com>
2018-01-16 17:07:34 +0900

    Create spa.js

---
+++ spa.js
@@ -0,0 +1,62 @@
+var content_id = ".content";
+
+$(function ($) {
+    'use strict';
+    if (window.history &amp;&amp; window.history.pushState) {
+        // click link with pushState
+        $(document).on('click', 'a:not([href^="#"], [class*="ignore"])', function(e) {
+            e.preventDefault();
+            var url = $(this).attr('href');
+            var title = $(this).attr('title');
+            history.pushState({url: url, title: title}, title, url);
+            getAction(url);
+        });
+        
+        // form submit with pushState
+        $(document).on('submit', 'form:not([action^="#"], [class*="ignore"])', function(e){
+            e.preventDefault();
+            var url = $(this).attr('action');
+            var title = $(this).attr('title');
+            history.pushState({url: url, title: title}, title, url);
+            postAction(url);
+        });
+        
+        // send get 
+        function getAction(url) {
+            $.get(url, { push_state: 'true' }, function(data) {
+                loadContent(data);
+            })
+            .fail(function() {
+                loadError();
+            });
+        }
+        
+        // send post
+        function postAction(url) {
+            $.post(url, $("form").serialize(), function(data){
+                loadContent(data);
+            })
+            .fail(function() {
+                loadError();
+            });
+        }
+        
+        // content
+        function loadContent(data) {
+	        var title = $(data).find('title').html();
+	        var content = $(data).find(content_id).html();
+	        $('title').html(title);
+	        $(content_id).html(content);
+        }
+        
+        // error
+        function loadError() {
+            location.href='/error' // or $(content_id).html('&lt;h1&gt;Error!&lt;/h1&gt;');
+        }
+        
+        // history back with popState
+        $(window).on('popstate', function(e){
+            getAction(location.pathname);
+        });
+    }
+});
